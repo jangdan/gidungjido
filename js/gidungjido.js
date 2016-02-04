bde33800b64bd6644806c515f21bbffd762b4f76
jangdan <minhyukjang@gmail.com>
2016-02-04 18:27:57 +0900

    fixed lighting
    plus: more elegant code and the object now spins

--- js/gidungjido.js
+++ js/gidungjido.js
@@ -2,7 +2,7 @@ var scene = new THREE.Scene();
 
 
 
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
 
 
@@ -18,16 +18,18 @@ document.body.appendChild(renderer.domElement);
 
 
 var countryShape = new THREE.Shape(); //a placeholder right triangle
-countryShape.moveTo( 1,  1);
-countryShape.lineTo( 1, -1);
-countryShape.lineTo(-1,  1);
-countryShape.lineTo( 1,  1);
+countryShape.moveTo( 1,  1 );
+countryShape.lineTo( 1, -1 );
+countryShape.lineTo(-1,  1 );
+countryShape.lineTo( 1,  1 );
 
 
 var countryGeometry = new THREE.ExtrudeGeometry(countryShape, { amount: 2, bevelEnabled: false } );
 
+countryGeometry.translate( 0, 0, -1 );
 
-var countryMaterial = new THREE.MeshPhongMaterial( { color: 0x3F6536 } );
+
+var countryMaterial = new THREE.MeshLambertMaterial( { color: 0x3F6536 } );
 
 
 var countryMesh = new THREE.Mesh(countryGeometry, countryMaterial);
@@ -35,12 +37,18 @@ var countryMesh = new THREE.Mesh(countryGeometry, countryMaterial);
 scene.add(countryMesh);
 
 
-
 //scene.add(new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshPhongMaterial( { color: 0xFF0000} ))); //debug
 
 
 
-//scene.add(new THREE.AmbientLight(0xFFFFFF));
+var spotlight = new THREE.SpotLight(0xFFFFD0, 1);
+
+spotlight.position.set( 10, 10, 10 );
+
+scene.add(spotlight);
+
+
+scene.add(new THREE.HemisphereLight( 0x444444, 0x444444 ));
 
 
 
@@ -51,6 +59,10 @@ camera.position.z = 10;
 function render(){
 
 	requestAnimationFrame(render);
+
+
+	countryMesh.rotation.x += 0.02;
+	countryMesh.rotation.y += 0.05;
 
 
 	renderer.render(scene, camera);
