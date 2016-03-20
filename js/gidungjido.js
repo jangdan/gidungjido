8edb8454d9b059153a2368d119b204f905979339
jangdan <minhyukjang@gmail.com>
2016-03-20 10:31:55 +0900

    line spacing

--- js/gidungjido.js
+++ js/gidungjido.js
@@ -636,15 +636,21 @@ function togglemenu(){
 function loadJSON(url, callback){   
 
 	var xobj = new XMLHttpRequest();
+	
 	xobj.overrideMimeType("application/json");
 
 	xobj.open('GET', url, true);
 
+
 	xobj.onreadystatechange = function(){
+
 		if(xobj.readyState == 4 && xobj.status == "200"){
+
 			// Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
 			callback(JSON.parse(xobj.responseText));
+
 		}
+
 	};
 
 	xobj.send(null);  
