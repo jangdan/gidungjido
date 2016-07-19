8bf3e7be7f78af8ff420443b3269ecf104d835fd
jangdan <minhyukjang@gmail.com>
2016-07-19 22:20:02 +0900

    translated menu options
    haven’t translated the option group labels (<optgroup>) because i have
    another wip branch that makes significant changes here; will do later

---
+++ english.js
@@ -0,0 +1,13 @@
+
+var MATERIAL_INDICIES = [
+	"MeshNormalMaterial",
+	"flags"
+];
+
+
+
+var PRELOADED_DATA_INDICIES = [
+	"Gross Domestic Product",
+	"Population"/*,
+	"Gross Domestic Product per Capita"*/
+];
--- js/gidungjido.js
+++ js/gidungjido.js
@@ -41,22 +41,6 @@ var PRESSED_KEYS = []; //keys that are currently pressed
 
 //DOM CONSTANTS
 
-var MATERIAL_INDICIES = [
-	"MeshNormalMaterial",
-	"flags"
-];
-
-
-
-var PRELOADED_DATA_INDICIES = [
-	"Gross Domestic Product",
-	"Population"/*,
-	"Gross Domestic Product per Capita"*/
-];
-
-
-
-
 var SLIDER_RESOLUTION = 10;
 
 
