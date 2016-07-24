67fe0daedbbe1ba240c668f8ed90b1e5636d6271
jangdan <minhyukjang@gmail.com>
2016-07-24 14:55:53 +0900

    internationalized the dataset selector

--- js/gidungjido.js
+++ js/gidungjido.js
@@ -184,41 +184,6 @@ scene.add( floor );
 
 //data
 
-var LOADABLE_DATASETS = [
-
-	{ name: "preloaded",
-
-		datasets: [
-			{ name: "Gross Domestic Product", index: 0 },
-			{ name: "Population", index: 1 }
-		]
-
-	},
-
-	{ name: "The World Bank",
-
-		datasets: [ //from http://data.worldbank.org/indicator
-
-			{ name: "GDP (current US$)", indicatorid: "NY.GDP.MKTP.CD", date: "2015" },
-			{ name: "GDP per capita (current US$)", indicatorid: "NY.GDP.PCAP.CD", date: "2015" },
-			{ name: "GDP growth (annual %)", indicatorid: "NY.GDP.MKTP.KD.ZG", date: "2015" },
-			{ name: "GDP per capita growth (annual %)", indicatorid: "NY.GDP.PCAP.KD.ZG", date: "2015" },
-			{ name: "Expense (% of GDP)", indicatorid: "GC.XPN.TOTL.GD.ZS", date: "2013" },
-
-			/*
-
-			//copy and fill in below for new World Bank datasets:
-			
-			{ name: "GDP", indicatorid: "asdf", date: "2015" },
-
-			*/
-
-		]
-
-	}
-
-];
-
 
 var datasets = []; //preloaded data + cached external API data
 
